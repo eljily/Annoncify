5b825268f1f490bb1603af694e1aa74bd50b5c1d
eljily <eljilysidibrahim@gmail.com>
2024-03-15 23:31:18 +0000

    added super admin role

--- src/main/java/com/sibrahim/annoncify/config/CorsConfig.java
+++ src/main/java/com/sibrahim/annoncify/config/CorsConfig.java
@@ -10,6 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowCredentials(false)
                 .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*");
--- src/main/java/com/sibrahim/annoncify/entity/enums/RoleEnum.java
+++ src/main/java/com/sibrahim/annoncify/entity/enums/RoleEnum.java
@@ -2,5 +2,6 @@ package com.sibrahim.annoncify.entity.enums;
 
 public enum RoleEnum {
     ADMIN,
-    USER
+    USER,
+    SUPER_ADMIN
 }
