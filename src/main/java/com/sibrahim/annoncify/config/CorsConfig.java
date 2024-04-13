05a0c836a95e9afe543e00ba8ad2398f2955ac4d
eljily <eljilysidibrahim@gmail.com>
2024-04-13 16:46:54 +0000

    updated cors configuration to works

--- src/main/java/com/sibrahim/annoncify/config/CorsConfig.java
+++ src/main/java/com/sibrahim/annoncify/config/CorsConfig.java
@@ -1,24 +1,23 @@
 package com.sibrahim.annoncify.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class AppCorsFilter extends CorsFilter {
-    public AppCorsFilter() {
-        super(configurationSource());
-    }
+public class CorsConfig {
 
-    private static UrlBasedCorsConfigurationSource configurationSource() {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
+        config.setAllowCredentials(false);
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-        return source;
+        return new CorsFilter(source);
     }
 }
