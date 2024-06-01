b3a79eb4f271efbcc54f7243ddfc95598f3208ff
eljily <eljilysidibrahim@gmail.com>
2024-06-01 13:36:51 +0000

    removed v1

--- src/main/java/com/sibrahim/annoncify/controller/CategoryController.java
+++ src/main/java/com/sibrahim/annoncify/controller/CategoryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/categories")
+@RequestMapping("/api/categories")
 @Slf4j
 public class CategoryController {
 
